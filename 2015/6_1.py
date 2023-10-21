7a5c91cfb591cd95f235eb1bacfacdb1a4a214dd
Mayank Patel <mayank_sesame@yahoo.com>
2023-10-20 21:34:25 -0400

    Faster fire hazard

--- 2015/6_1.py
+++ 2015/6_1.py
@@ -2,42 +2,51 @@
 # https://adventofcode.com/2015/day/6
 
 import numpy as np
+from utils import timeit
 
 light_grid = np.zeros((1000, 1000), dtype=int)
 
 
-def turn_on(i, j):
-    light_grid[i][j] |= 1
+def turn_on(left_corner, right_corner):
+    light_grid[
+        left_corner[0] : right_corner[0] + 1, left_corner[1] : right_corner[1] + 1
+    ] |= 1
 
 
-def turn_off(i, j):
-    light_grid[i][j] &= 0
+def turn_off(left_corner, right_corner):
+    light_grid[
+        left_corner[0] : right_corner[0] + 1, left_corner[1] : right_corner[1] + 1
+    ] &= 0
 
 
-def toggle(i, j):
-    if light_grid[i][j] == 0:
-        light_grid[i][j] = 1
-    else:
-        light_grid[i][j] = 0
+def toggle(left_corner, right_corner):
+    light_grid[
+        left_corner[0] : right_corner[0] + 1, left_corner[1] : right_corner[1] + 1
+    ] = (
+        1
+        - light_grid[
+            left_corner[0] : right_corner[0] + 1, left_corner[1] : right_corner[1] + 1
+        ]
+    )
 
 
-def update_light_grid(left_corner, right_corner, action):
-    for i in range(left_corner[0], right_corner[0] + 1):
-        for j in range(left_corner[1], right_corner[1] + 1):
-            action(i, j)
-
-
-with open("2015/6.input", "r") as f:
-    for line in f:
-        instructions = line.strip().split(" ")
-        right_corner = [eval(x) for x in instructions[-1].split(",")]
-        left_corner = [eval(x) for x in instructions[-3].split(",")]
-        if instructions[0] == "turn":
-            if instructions[1] == "on":
-                update_light_grid(left_corner, right_corner, turn_on)
+@timeit
+def main():
+    with open("2015/6.input", "r") as f:
+        for line in f:
+            instructions = line.strip().split(" ")
+            right_corner = [eval(x) for x in instructions[-1].split(",")]
+            left_corner = [eval(x) for x in instructions[-3].split(",")]
+            if instructions[0] == "turn":
+                if instructions[1] == "on":
+                    turn_on(left_corner, right_corner)
+                else:
+                    turn_off(left_corner, right_corner)
             else:
-                update_light_grid(left_corner, right_corner, turn_off)
-        else:
-            update_light_grid(left_corner, right_corner, toggle)
+                toggle(left_corner, right_corner)
 
-print(light_grid.sum())
+    print(light_grid.sum())
+
+
+if __name__ == "__main__":
+    main()
